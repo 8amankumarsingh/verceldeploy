930bdc8f745784752b60b64b0a97abc77c54acdc
8amankumarsingh <135354643+8amankumarsingh@users.noreply.github.com>
2025-02-11 03:22:26 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className='header'>
             <div className='header-contents'>
                 
-                <p></p>
+                <p> </p>
                 <p>It's our promise. Experience the convenience of having fresh produce, pantry staples, and restaurant-quality meals delivered right to your doorstep, always on schedule.</p>
             </div>
         </div>
